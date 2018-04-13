e897407b7a9c4ccc87a19a2d13067e3976e3da8d
Yusuke <y_doi@nuee.nagoya-u.ac.jp>
2018-04-13 21:15:59 +0900

    Implement advanced login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,4 @@
+ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require "minitest/reporters"
@@ -10,5 +11,16 @@ class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
   def is_logged_in?
     !session[:user_id].nil?
+  end
+end
+
+
+class ActionDispatch::IntegrationTest
+
+  # テストユーザーとしてログインする
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
   end
 end
